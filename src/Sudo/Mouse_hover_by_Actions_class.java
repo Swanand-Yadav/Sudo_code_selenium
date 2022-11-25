c2ee2e0e900317c1cd2350980741557a33c2b49a
Swanand-Yadav <swanandtyadav@gmail.com>
2022-11-25 11:44:50 +0530

    actions class added

---
+++ src/Sudo/Mouse_hover_by_Actions_class.java
@@ -0,0 +1,23 @@
+package Sudo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+;
+
+public class Mouse_hover_by_Actions_class 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.onlinesbi.sbi/");
+		driver.manage().window().maximize();
+		Actions action=new Actions(driver);
+		Thread.sleep(3000);
+		action.moveToElement(driver.findElement(By.xpath("//a[text()='Services ']"))).build().perform();
+		
+	}
+}
