76b22f86e6178ef4bc77089ea26bb1edd619c3c1
Swanand-Yadav <swanandtyadav@gmail.com>
2023-03-14 23:51:52 +0530

    added

--- src/JavaScriptExecutor/Refreshing_and_reloading_browser.java
+++ src/JavaScriptExecutor/Refreshing_and_reloading_browser.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Refreshing_browser 
+public class Refreshing_and_reloading_browser 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
 		ChromeOptions co = new ChromeOptions();
@@ -15,7 +15,9 @@ public class Refreshing_browser
 		WebDriver driver=new ChromeDriver(co);
 		
 		driver.get("https://www.studytonight.com/data-structures/introduction-to-sorting");
+		Thread.sleep(3000);
 		JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("history.go(0)");
+		js.executeScript("history.go(0)"); // It will refreshing the page.
+		js.executeScript("location.reload()");// It will reloading the page.
 	}
 }
