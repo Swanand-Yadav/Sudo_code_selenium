431d7480f4befbb83c6c2591696a7a2b5d05f2ff
Swanand-Yadav <swanandtyadav@gmail.com>
2023-03-15 00:28:39 +0530

    origin master

--- src/JavaScriptExecutor/Scrolling_window_by_specific_number.java
+++ src/JavaScriptExecutor/Scrolling_window_by_specific_number.java
@@ -1,15 +1,18 @@
-package Sudo;
+package JavaScriptExecutor;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Scrolling_window 
+public class Scrolling_window_by_specific_number 
 {
 	public static void main(String[] args) 
 	{
 		System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(co);
 		
 		driver.get("https://randomuser.me/");
 		
